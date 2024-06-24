88f1bb9099c7f8a49b6e61c81b8da0560cd5a4a9
JagrutiTatar <86821720+JagrutiTatar@users.noreply.github.com>
2024-06-24 14:01:06 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 // src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './component/Home';
-import About from './component/About';
-import Profile from './component/Profile';
-import Cart from './component/Cart';
 
+const Home = () => {
+  return <h1>Home Page</h1>;
+};
+
+const About = () => {
+  return <h1>About Page</h1>;
+};
+
+const Cart = () => {
+  return <h1>Cart Page</h1>;
+};
+ 
 function App() {
   return (
     <Router>
       <div>
         <Routes>
-          <Route path="/" element={<Cart />} />
           <Route path="/about" element={<About />} />
-          <Route path="/profile/:userId" element={<Profile />} />
+          <Route path="/:cart" element={<Cart />} />
+          <Route path="/home" element={<Home />} />
         </Routes>
       </div>
     </Router>
