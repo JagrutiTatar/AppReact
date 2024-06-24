7786a37403a0a760f157514549a3eca227570eac
JagrutiTatar <tatarjagruti08@gmail.com>
2024-06-24 13:13:43 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -4,18 +4,14 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './component/Home';
 import About from './component/About';
 import Profile from './component/Profile';
+import Cart from './component/Cart';
 
 function App() {
   return (
     <Router>
       <div>
-        <nav>
-          <a href="/">Home</a>
-          <a href="/about">About</a>
-          <a href="/profile/1">Profile</a>
-        </nav>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Cart />} />
           <Route path="/about" element={<About />} />
           <Route path="/profile/:userId" element={<Profile />} />
         </Routes>
